a7448886dd6da2d051ca1ea5aa021be3f0429459
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-11-19 02:12:40 -0700

    Fix main container layout issues, improve about page layout

--- web/src/vendors/fonts.ts
+++ web/src/vendors/fonts.ts
@@ -1,5 +1,6 @@
 require('typeface-droid-sans');
+require('typeface-nunito');
+require('typeface-quicksand');
+require('typeface-raleway');
 require('typeface-roboto');
 require('typeface-ubuntu');
-require('typeface-raleway');
-require('typeface-quicksand');
