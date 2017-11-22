55b63794112d4e8c14f3a5939d267db1978ee540
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-11-21 20:46:34 -0700

    Stylize skill component

--- web/src/features/skills/skill/skill.component.ts
+++ web/src/features/skills/skill/skill.component.ts
@@ -2,7 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'mt-skill',
-    templateUrl: 'skill.component.html'
+    templateUrl: 'skill.component.html',
+    styleUrls: ['skill.component.css']
 })
 
 export class MTSkillComponent implements OnInit {
