1cd0555ae7e7ad6dfac40fb3f7c881a1591477d9
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-11-21 22:58:45 -0700

    Finish adding certifications

--- web/src/features/certifications/certifications.component.ts
+++ web/src/features/certifications/certifications.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {certifications} from './static/certification-definitions';
 
 @Component({
     selector: 'mt-certifications',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class MTCertificationsComponent implements OnInit {
+
+    certifications = certifications;
 
     constructor() {
     }
---
+++ web/src/features/certifications/static/certification-definitions.ts
@@ -0,0 +1,63 @@
+export const certifications: [{title: string, images: string[]}] = [
+    {
+        title: "MTA – Software Development Fundamentals (C#)",
+        images: [
+            require("./images/SoftwareDevelopmentFundamentals.jpg")
+        ]
+    },
+    {
+        title: "MTA – Security Fundamentals",
+        images: [
+            require("./images/SecurityFundamentals.jpg")
+        ]
+    },
+    {
+        title: "MTA – HTML5 App Development",
+        images: [
+            require("./images/HTML5ApplicationDevelopmentFundamentals.jpg")
+        ]
+    },
+    {
+        title: "CTE – Computer Programming",
+        images: [
+            require("./images/Skill_Programming_1.jpg"),
+            require("./images/Skill_Programming_2.jpg")
+        ]
+    },
+    {
+        title: "CTE – Foundations of Tech",
+        images: [
+            require("./images/Skill_Foundations_Of_Tech_1.jpg"),
+            require("./images/Skill_Foundations_Of_Tech_2.jpg")
+        ]
+    },
+    {
+        title: "CTE – Basic Electronics",
+        images: [
+            require("./images/Skill_Electronics_1.jpg"),
+            require("./images/Skill_Electronics_2.jpg")
+        ]
+    },
+    {
+        title: "CTE – Woodworking",
+        images: [
+            require("./images/Skill_Woodworking_1.jpg"),
+            require("./images/Skill_Woodworking_2.jpg")
+        ]
+    },
+    {
+        title: "CTE – 3D Graphics",
+        images: [
+            require("./images/Skill_3DGraphics_1.jpg"),
+            require("./images/Skill_3DGraphics_2.jpg")
+        ]
+    },
+    {
+        title: "CTE – Automotive Service",
+        images: [
+            require("./images/Skill_Auto_1.jpg"),
+            require("./images/Skill_Auto_2.jpg")
+        ]
+    },
+];
+
