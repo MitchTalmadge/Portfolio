4c6163e156dab1dcbbc37a50b8b6efbf7acd22df
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-11-21 22:40:56 -0700

    Add image previous/next capabilities to showcase

--- web/src/shared/showcase/showcase.component.ts
+++ web/src/shared/showcase/showcase.component.ts
@@ -14,9 +14,34 @@ export class MTShowcaseComponent implements OnInit {
     @Input()
     public images: string[] = [];
 
+    /**
+     * The index of the current image being displayed from the images array.
+     */
+    public currentImageIndex: number = 0;
+
     constructor() {
     }
 
     ngOnInit() {
+    }
+
+    /**
+     * Displays the previous image.
+     */
+    public previousImage(): void {
+        if (this.currentImageIndex == 0)
+            this.currentImageIndex = this.images.length - 1;
+        else
+            this.currentImageIndex -= 1;
+    }
+
+    /**
+     * Displays the next image.
+     */
+    public nextImage(): void {
+        if (this.currentImageIndex == this.images.length - 1)
+            this.currentImageIndex = 0;
+        else
+            this.currentImageIndex += 1;
     }
 }
