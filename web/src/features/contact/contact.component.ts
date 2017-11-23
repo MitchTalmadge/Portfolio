f666e1b2e6ddd491bea2213df65c53caab28b7eb
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-11-22 23:50:29 -0700

    Begin creating contact form

--- web/src/features/contact/contact.component.ts
+++ web/src/features/contact/contact.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'mt-contact',
-    templateUrl: 'contact.component.html'
+    templateUrl: 'contact.component.html',
+    styleUrls: ['contact.component.css']
 })
 
 export class MTContactComponent implements OnInit {
